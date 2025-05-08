370ff0789246aaa2d7ae9c4edf939597be233cc1
Sebastian Luchetta <luchetta.hs@gmail.com>
2025-05-08 01:46:51 -0300

    -Inicio V1.1: agregado los vencimientos -Nuevo alert al guardar un perfil nuevo. -agregado el verbo post en el controller de perfil y en el form de nuevo perfil

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const alertPlaceholder = document.getElementById('PlaceHolderAlert') //Espacio donde dibuja el alert
+const alertTrigger = document.getElementById('BtnGuardar') //Boton del form
+const formulario = document.getElementById('NuevoPerfilForm'); //Formulario
 
-// Write your JavaScript code.
+//Dibuja el alert cuando invodada
+function alert(message, type) {
+    var Contenido = document.createElement('div')
+    Contenido.innerHTML = '<div class="alert alert-'
+        + type +
+        ' alert-dismissible" role="alert">'
+        + message
+        + '<button type="submit" class="btn-close" data-bs-dismiss="alert" aria-label="Close" onclick="enviarFormulario()" > </button></div>'
+
+    alertPlaceholder.append(Contenido)
+}
+//Hace submit del form, se invoca desde el boton de cerrar del alert
+function enviarFormulario() {
+    formulario.submit();
+}
+//Al boton del form, al hacer clic, invoca a la funcion alert
+if (alertTrigger) {
+    alertTrigger.addEventListener('click', function () {
+        alert('Perfil creado exitosamente', 'success')
+    })
+}
